f492769bd588c55dd5ed839fd737f29dd2e255aa
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2020-05-30 17:16:51 +0700

    Make questions page responsive

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,57 +1,42 @@
 import React from "react";
 
 import { Typography, TextField, Grid, Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-  field: {
-    width: "35%",
-    marginTop: 25,
-  },
-  button: {
-    marginTop: 25,
-  },
-});
 
 export default function Question() {
-  const styles = useStyles();
-
   return (
-    <>
-      <Typography variant="h2">Ask a question</Typography>
-      <Grid>
-        <Grid item>
-          <TextField className={styles.field} label="Site" variant="outlined" />
-        </Grid>
-        <Grid item>
-          <TextField
-            className={styles.field}
-            label="Your speciality"
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            className={styles.field}
-            label="Query subject"
-            variant="outlined"
-            required
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            className={styles.field}
-            label="Query"
-            variant="outlined"
-            rows={10}
-            required
-            multiline
-          />
-        </Grid>
-        <Button variant="contained" className={styles.button} color="primary">
+    <Grid container spacing={2} direction="column">
+      <Typography style={{ margin: "16px 8px" }} variant="h2">
+        Ask a question
+      </Typography>
+      <Grid item xs={12} md={6}>
+        <TextField label="Site" variant="outlined" fullWidth />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <TextField label="Your speciality" variant="outlined" fullWidth />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <TextField
+          label="Query subject"
+          variant="outlined"
+          fullWidth
+          required
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <TextField
+          label="Query"
+          variant="outlined"
+          rows={10}
+          fullWidth
+          required
+          multiline
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" color="primary">
           Submit
         </Button>
       </Grid>
-    </>
+    </Grid>
   );
 }
