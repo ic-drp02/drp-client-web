66f6c4f89eefe4caa895ee2629e84f2ce8ece33d
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2020-06-02 20:03:03 +0700

    Fix incorrect function name

--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -4,7 +4,7 @@ import { Typography, Grid } from "@material-ui/core";
 
 import PostSummary from "../components/PostSummary.js";
 
-export default function Admin() {
+export default function AllPosts() {
   return (
     <Grid container spacing={2} direction="row-reverse">
       <Grid item xs={12} md={3}>
