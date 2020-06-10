e66c06258e0f87237e5fa8819f7a87236e7b09a8
Hasan Ali <git@hasali.co.uk>
2020-06-10 21:25:43 +0100

    Implement login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useCallback } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import React, { useState, useCallback, useContext } from "react";
+import { Switch, Route, Link, Redirect } from "react-router-dom";
 
 import SearchIcon from "@material-ui/icons/Search";
 import {
@@ -10,6 +10,7 @@ import {
   Snackbar,
 } from "@material-ui/core";
 
+import Login from "./pages/Login";
 import Home from "./pages/Home.js";
 import Question from "./pages/Question.js";
 import CreatePost from "./pages/CreatePost.js";
@@ -24,9 +25,28 @@ import AdminSubjects from "./pages/AdminSubjects.js";
 import logo from "./assets/icon_logo.png";
 import logo_text from "./assets/icon_text.png";
 
+import AuthContext from "./AuthContext";
 import SnackbarContext from "./SnackbarContext";
 
+function AuthRoute({ component, ...rest }) {
+  const Component = component;
+  const auth = useContext(AuthContext);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        !!auth.user ? (
+          <Component />
+        ) : (
+          <Redirect to={{ pathname: "/login", state: { from: location } }} />
+        )
+      }
+    />
+  );
+}
+
 export default function App() {
+  const [user, setUser] = useState(null);
   const [snackbar, setSnackbar] = useState({
     open: false,
     show(message, duration) {
@@ -40,52 +60,65 @@ export default function App() {
   );
 
   return (
-    <SnackbarContext.Provider value={snackbar}>
-      <AppBar position="sticky" color="default">
-        <Toolbar>
-          <Link to="/">
-            <span>
-              <img height="24" src={logo} alt="" style={{ marginRight: 12 }} />
-              <img height="24" src={logo_text} alt="" />
-            </span>
-          </Link>
-          <section style={{ marginLeft: "auto" }}>
-            <IconButton aria-label="search">
-              <SearchIcon />
-            </IconButton>
-            <Link to="/admin" style={{ textDecoration: "none" }}>
-              <Button variant="outlined" color="primary">
-                Admin Dashboard
-              </Button>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <SnackbarContext.Provider value={snackbar}>
+        <AppBar position="sticky" color="default">
+          <Toolbar>
+            <Link to="/">
+              <span>
+                <img
+                  height="24"
+                  src={logo}
+                  alt=""
+                  style={{ marginRight: 12 }}
+                />
+                <img height="24" src={logo_text} alt="" />
+              </span>
             </Link>
-          </section>
-        </Toolbar>
-      </AppBar>
-      <div style={{ padding: 25 }}>
-        <Switch>
-          <Route path="/admin/updates" component={AdminUpdates} />
-          <Route path="/admin/tags" component={AdminTags} />
-          <Route path="/admin/questions/sites" component={AdminSites} />
-          <Route path="/admin/questions/subjects" component={AdminSubjects} />
-          <Route path="/admin/questions" component={AdminQuestions} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/posts/create" component={CreatePost} />
-          <Route path="/posts" component={AllPosts} />
-          <Route path="/question" component={Question} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </div>
-      <Snackbar
-        open={snackbar.open}
-        message={snackbar.message}
-        autoHideDuration={snackbar.duration}
-        onClose={hideSnackbar}
-        action={
-          <Button color="secondary" size="small" onClick={hideSnackbar}>
-            hide
-          </Button>
-        }
-      />
-    </SnackbarContext.Provider>
+            {!!user && user.role === "admin" && (
+              <section style={{ marginLeft: "auto" }}>
+                <IconButton aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+                <Link to="/admin" style={{ textDecoration: "none" }}>
+                  <Button variant="outlined" color="primary">
+                    Admin Dashboard
+                  </Button>
+                </Link>
+              </section>
+            )}
+          </Toolbar>
+        </AppBar>
+        <div style={{ padding: 25 }}>
+          <Switch>
+            <AuthRoute path="/admin/updates" component={AdminUpdates} />
+            <AuthRoute path="/admin/tags" component={AdminTags} />
+            <AuthRoute path="/admin/questions/sites" component={AdminSites} />
+            <AuthRoute
+              path="/admin/questions/subjects"
+              component={AdminSubjects}
+            />
+            <AuthRoute path="/admin/questions" component={AdminQuestions} />
+            <AuthRoute path="/admin" component={Admin} />
+            <AuthRoute path="/posts/create" component={CreatePost} />
+            <AuthRoute path="/posts" component={AllPosts} />
+            <AuthRoute path="/question" component={Question} />
+            <Route path="/login" component={Login} />
+            <AuthRoute path="/" component={Home} />
+          </Switch>
+        </div>
+        <Snackbar
+          open={snackbar.open}
+          message={snackbar.message}
+          autoHideDuration={snackbar.duration}
+          onClose={hideSnackbar}
+          action={
+            <Button color="secondary" size="small" onClick={hideSnackbar}>
+              hide
+            </Button>
+          }
+        />
+      </SnackbarContext.Provider>
+    </AuthContext.Provider>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,160 @@
+import React, { useState, useContext } from "react";
+
+import {
+  Grid,
+  Typography,
+  TextField,
+  Paper,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+
+import AuthContext from "../AuthContext";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  card: {
+    width: 430,
+  },
+  "@media (max-width: 720px)": {
+    card: {
+      width: "60%",
+    },
+  },
+  "@media (max-width: 480px)": {
+    card: {
+      width: "100%",
+    },
+  },
+});
+
+export default function Login() {
+  const classes = useStyles();
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function login(e) {
+    e.preventDefault();
+
+    let res;
+    try {
+      res = await fetch("/auth/authenticate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+    } catch {
+      setError({
+        type: "Unknown",
+        message: "An eerror occurred while communicating with the server.",
+      });
+    }
+
+    const body = await res.json();
+
+    if (res.status !== 200) {
+      if (!!body.type) {
+        setError(body);
+      } else {
+        setError({
+          type: "Unknown",
+          message: "An error occurred while trying to log in.",
+        });
+      }
+    } else {
+      auth.setUser(body);
+      history.replace("/");
+    }
+  }
+
+  let errorMessage;
+  if (!!error) {
+    switch (error.type) {
+      case "InvalidCredentials":
+        errorMessage = "The email address or password is incorrect.";
+        break;
+
+      case "Unconfirmed":
+        errorMessage =
+          "Account has not been confirmed. Check your email for a confirmation link.";
+        break;
+
+      case "Unknown":
+        errorMessage = error.message;
+        break;
+    }
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: 24,
+      }}
+    >
+      <form action="#" onSubmit={login}>
+        <Paper style={{ padding: 16 }} className={classes.card}>
+          <Grid container spacing={3} direction="column">
+            <Grid item xs={12}>
+              <Typography variant="h4" style={{ textAlign: "center" }}>
+                Login
+              </Typography>
+            </Grid>
+            {!!errorMessage && (
+              <Grid item xs={12}>
+                <Typography style={{ textAlign: "center", color: "red" }}>
+                  {errorMessage}
+                </Typography>
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                variant="outlined"
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                variant="outlined"
+                fullWidth
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                type="submit"
+              >
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </form>
+    </div>
+  );
+}
